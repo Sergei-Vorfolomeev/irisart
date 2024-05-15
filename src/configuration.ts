32e7a3783ee9916c846109674580ac7a497f794c
Sergei-Vorfolomeev <vorfo1897@gmail.com>
2024-05-15 19:19:15 +0300

    connect postgres and setup typeorm

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,8 +2,14 @@ import * as process from 'process'
 import { ConfigService } from '@nestjs/config'
 
 const config = () => ({
-  ENV: process.env.NODE_ENV,
-  PORT: parseInt(process.env.PORT) || 5000,
+  env: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT) || 5000,
+  db: {
+    name: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
 })
 
 export type ConfigType = ReturnType<typeof config>
